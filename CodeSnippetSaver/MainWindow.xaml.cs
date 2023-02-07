5075c59ba43d1952c17a6e7b7646b4a5deb570d9
FTyumin <feodor.tjumin28@gmail.com>
2023-02-07 21:19:09 +0200

    Small edits

--- CodeSnippetSaver/MainWindow.xaml.cs
+++ CodeSnippetSaver/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace CodeSnippetSaver
         {
             InitializeComponent();
         }
+
+        private void okButton_Click(object sender, RoutedEventArgs e) =>
+            DialogResult = true;
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e) =>
+            DialogResult = false;
+
     }
 }
